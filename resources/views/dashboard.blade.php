41588dbd68d9f6d2878b805c1fd268a8e46feef3
Rifjan Jundila <rjndla@gmail.com>
2022-11-08 21:54:37 +0700

    feat: routing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,157 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+@extends('partials.template')
+@section('content')
+    <div class="content-body">
+        <!-- row -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card">
+                        <div class="stat-widget-two card-body">
+                            <div class="stat-content">
+                                <div class="stat-text">Today Expenses </div>
+                                <div class="stat-digit"> <i class="fa fa-usd"></i>8500</div>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success w-85" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card">
+                        <div class="stat-widget-two card-body">
+                            <div class="stat-content">
+                                <div class="stat-text">Income Detail</div>
+                                <div class="stat-digit"> <i class="fa fa-usd"></i>7800</div>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-primary w-75" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card">
+                        <div class="stat-widget-two card-body">
+                            <div class="stat-content">
+                                <div class="stat-text">Task Completed</div>
+                                <div class="stat-digit"> <i class="fa fa-usd"></i> 500</div>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-warning w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card">
+                        <div class="stat-widget-two card-body">
+                            <div class="stat-content">
+                                <div class="stat-text">Task Completed</div>
+                                <div class="stat-digit"> <i class="fa fa-usd"></i>650</div>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-danger w-65" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /# card -->
+                </div>
+                <!-- /# column -->
+            </div>
+            <div class="row">
+                <div class="col-xl-8 col-lg-8 col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Sales Overview</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-xl-12 col-lg-8">
+                                    <div id="morris-bar-chart"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Project</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="current-progress">
+                                <div class="progress-content py-2">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="progress-text">Website</div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="current-progressbar">
+                                                <div class="progress">
+                                                    <div class="progress-bar progress-bar-primary w-40" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+                                                        40%
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="progress-content py-2">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="progress-text">Android</div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="current-progressbar">
+                                                <div class="progress">
+                                                    <div class="progress-bar progress-bar-primary w-60" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                                                        60%
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="progress-content py-2">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="progress-text">Ios</div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="current-progressbar">
+                                                <div class="progress">
+                                                    <div class="progress-bar progress-bar-primary w-70" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                                                        70%
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="progress-content py-2">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="progress-text">Mobile</div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="current-progressbar">
+                                                <div class="progress">
+                                                    <div class="progress-bar progress-bar-primary w-90" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                                        90%
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+    Route::prefix('dashboard')->group(function () {
+        Route::resource('token',TokenController::class);
+    });
 
+});
 require __DIR__.'/auth.php';
