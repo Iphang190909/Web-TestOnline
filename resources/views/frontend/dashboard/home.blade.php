76127633a5fc0a9ac678f2330ce021c01fd796e7
Iphang190909 <72132369+Iphang190909@users.noreply.github.com>
2022-11-19 01:39:36 +0700

    Update home.blade.php

--- resources/views/frontend/dashboard/home.blade.php
+++ resources/views/frontend/dashboard/home.blade.php
@@ -1,12 +1,12 @@
 @extends('frontend.template')
 @section('content')
-<div class="row mx-auto my-auto">
+<div class="container mx-auto my-auto">
   <div class="col-lg-6 mt-5">
-      <h3 class="mt-5">Selamat anda berhasil login di halaman utama <strong>Tes Online</strong></h3>
+      <h1 class="mt-5">Selamat anda berhasil login di halaman utama <strong>Tes Online</strong></h1>
       <p class="mb-4">Pilih selanjutnya agar anda bisa mendapatkan token untuk memulai ujian</p>
-      <a href="{{route('InputToken')}}" class="btn btn-primary">Lanjutkan</a>
+      <a href="{{route('InputToken')}}" class="btn btn-primary text-center btn-lg">Lanjutkan</a>
     </div>
-    <div class="col-lg-6 ">
+    <div class="col-lg-6">
         <img  class="img-fluid " src="{{asset('images/tes_mbti2.png')}}" alt="" srcset="" >
     </div>
   </div>
